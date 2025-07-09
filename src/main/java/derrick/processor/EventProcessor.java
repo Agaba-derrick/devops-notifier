dbda334117a0e04fdf35c13721bae0efeba642cb
Agaba Derrick <agabaderrick18@gmail.com>
2025-07-09 19:56:42 +0300

    add event processor package
    Signed-off-by: Agaba Derrick <agabaderrick18@gmail.com>

---
+++ src/main/java/derrick/processor/EventProcessor.java
@@ -0,0 +1,20 @@
+package com.derick.notificationbot.processor;
+
+/**
+ * EventProcessor defines the contract for processing different types of CI/CD events.
+ */
+public interface EventProcessor {
+
+    /**
+     * Checks if this processor supports the given event type.
+     * @param eventType The event type (e.g. pull_request, build_failure)
+     * @return true if supported, false otherwise
+     */
+    boolean supports(String eventType);
+
+    /**
+     * Processes the given event payload.
+     * @param payload The event payload as a String (typically JSON)
+     */
+    void processEvent(String payload);
+}
