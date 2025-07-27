fbc4b3c31d68bb4406a9f287ce22ef9ed0dbef1b
Agaba Derrick <agabaderrick18@gmail.com>
2025-07-28 00:16:40 +0300

    add event processor and logger implementations
    Signed-off-by: Agaba Derrick <agabaderrick18@gmail.com>

--- src/main/java/derrick/Notificationbot/rest/HelloController.java
+++ src/main/java/derrick/Notificationbot/rest/HelloController.java
@@ -1,4 +1,4 @@
-package com.derick.notificationbot;
+package derrick.Notificationbot.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
